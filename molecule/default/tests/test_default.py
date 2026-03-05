f8c96c0860c5afd4edb7ba2fb2d5d70c5693140c
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2026-03-05 17:12:42 -0500

    Update service dropin to look for agent name tag in config before starting
    Also add a gate file to ensure that the service is only started the
    first time the config is modified.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -2,7 +2,6 @@
 
 # Standard Python Libraries
 import os
-import re
 
 # Third-Party Libraries
 import pytest
@@ -45,36 +44,3 @@ def test_dropin_file(host):
     assert f.user == "root"
     assert f.group == "root"
     assert f.mode == 0o644
-
-
-@pytest.mark.parametrize(
-    "prop,regex",
-    [
-        ("After", r"^After=.*cloud-final\.service"),
-        ("Wants", r"^Wants=.*cloud-final\.service"),
-    ],
-)
-def test_unit_properties(host, prop, regex):
-    """Test that unit properties were modified via drop-in as expected."""
-    cmd = f"systemctl show --no-pager --property={prop} wazuh-agent.service"
-    cmd_result = host.run(cmd)
-    assert cmd_result.rc == 0, f"{cmd} command failed"
-    assert (
-        re.search(regex, cmd_result.stdout) is not None
-    ), f"Regex {regex} does not match any line in {cmd} output."
-
-
-@pytest.mark.parametrize(
-    "unit,dependent_unit",
-    [
-        ("multi-user.target", "wazuh-agent.service"),
-    ],
-)
-def test_dependency(host, unit, dependent_unit):
-    """Test that dependent_unit is a dependency of unit."""
-    cmd = f"systemctl list-dependencies {unit}"
-    cmd_result = host.run(cmd)
-    assert cmd_result.rc == 0, f"{cmd} command failed"
-    assert (
-        dependent_unit in cmd_result.stdout
-    ), f"{dependent_unit} not listed as a dependency of {unit}."
