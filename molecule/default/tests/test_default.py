2db5603f3fed44f4fe45d885e335c4872e28522f
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2026-03-05 11:05:17 -0500

    Add a test to ensure that wazuh-agent is a dependency of multi-user.target
    This verifies that the wazuh-agent service will be started at boot
    when enabled.  This test would have failed before this PR, so it makes
    sense to add it to ensure there are no regressions.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -62,3 +62,19 @@ def test_unit_properties(host, prop, regex):
     assert (
         re.search(regex, cmd_result.stdout) is not None
     ), f"Regex {regex} does not match any line in {cmd} output."
+
+
+@pytest.mark.parametrize(
+    "unit,dependent_unit",
+    [
+        ("multi-user.target", "wazuh-agent.service"),
+    ],
+)
+def test_dependency(host, unit, dependent_unit):
+    """Test that dependent_unit is a dependency of unit."""
+    cmd = f"systemctl list-dependencies {unit}"
+    cmd_result = host.run(cmd)
+    assert cmd_result.rc == 0, f"{cmd} command failed"
+    assert (
+        "wazuh-agent.service" in cmd_result.stdout
+    ), f"{dependent_unit} not listed as a dependency of {unit}."
